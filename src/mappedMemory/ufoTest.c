3f1bb629c9be1e1fb89a1f08b68b8dacf0e722fc
Colette 🌩️ 💝 Kerr <ckerr@mdnwinsite.com>
2019-05-29 15:35:08 +0200

    Removed a missed merge conflit

--- src/mappedMemory/ufoTest.c
+++ src/mappedMemory/ufoTest.c
@@ -30,26 +30,30 @@ int testpopulate(uint64_t startValueIdx, uint64_t endValueIdx, ufPopulateCallout
 int main(int argc, char **argv) {
   int res;
 
+  srand(13);
 
   ufInstance_t ufI = ufMakeInstance();
   tryPerrInt(res, ufInit(ufI), "Err Init", error);
 
-  ufObjectConfig_t config = makeObjectConfig(uint64_t, 64, 1024ull*1024*1024, 1024*1024);
-  ufSetPopulateFunction(config, testpopulate);
-
 //  uint64_t* bitmap = calloc(1024*1024*1024, 8);
   do{
+    const uint64_t ct = 1024ull*1024*((rand() & 0xffull) + 1), sz = ct*8 ;
+
+    ufObjectConfig_t config = makeObjectConfig(uint64_t, 64, ct, (rand() & 0xffff) + 1);
+    ufSetPopulateFunction(config, testpopulate);
+
     ufObject_t o;
     tryPerrInt(res, ufCreateObject(ufI, config, &o), "Err init obj", error1);
     uint64_t* ptr = (uint64_t*) ufGetValuePointer(o);
 
-    const uint64_t ct = 1024ull*1024*1024, sz = ct*8 ;
+    free(config);
+
+    printf("%lu\n", (uint64_t)ct);
 
     while((random() & 0xfff) != 0){
       uint64_t i = rand() % ct;
       assert(i < ct);
       assert(  (((uint64_t) &ptr[i]) - ((uint64_t)ptr)) < sz );
-      printf("%lu\n", i);
       uint64_t x = ptr[i];
       assert(~i == x);
     }
