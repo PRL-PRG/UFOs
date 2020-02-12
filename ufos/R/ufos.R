5d6666baadf4c20e328459fdde696db6e8a4b204
Konrad Siek <konrad.siek@gmail.com>
2020-02-12 15:17:24 +0100

    Invisible return from onUnload.

--- ufos/R/ufos.R
+++ ufos/R/ufos.R
@@ -1,11 +1,13 @@
+# Kills the UFO framework.
+jeff_goldbloom <- function(...) invisible(.Call("ufo_shutdown"))
+
 # Finalizer for the entire UFO framework, called on session exit.
 .onLoad <- function(libname, pkgname) {
-  jeff_goldbloom <- function(...) invisible(.Call("ufo_shutdown"))
   invisible(reg.finalizer(.GlobalEnv, jeff_goldbloom, onexit = TRUE))
 }
 
 # Finalizer for the entire UFO framework, called on package unload.
 # AFAIK this cannot be relied on to be called on shutdown.
 .onUnload <- function(libname, pkgname) {
-  .Call("ufo_shutdown")
+  jeff_goldbloom()
 }
