a2dcf17709e454bcaac16274da29a16906082ee6
Konrad Siek <konrad.siek@gmail.com>
2020-02-14 12:13:02 +0100

    Slight cleanup.

--- ufos/src/init.c
+++ ufos/src/init.c
@@ -16,15 +16,8 @@ static const R_CallMethodDef CallEntries[] __attribute__ ((unused)) = {
 // Initializes the package and registers the routines with the Rdynload 
 // library. Name follows the pattern: R_init_<package_name> 
 void attribute_visible R_init_ufos(DllInfo *dll) {
-    //InitUFOAltRepClass(dll);
-    //R_registerRoutines(dll, NULL, CallEntries, NULL, NULL);
-    //R_useDynamicSymbols(dll, FALSE);
-    //R_forceSymbols(dll, TRUE);
-
-    //R_RegisterCCallable("ufos", "ufo_initialize", (DL_FUNC) &ufo_initialize);
     R_RegisterCCallable("ufos", "ufo_new", (DL_FUNC) &ufo_new);
     R_RegisterCCallable("ufos", "ufo_new_multidim", (DL_FUNC) &ufo_new_multidim);
-    //R_RegisterCCallable("ufos", "ufo_shutdown", (DL_FUNC) &ufo_shutdown);
 }
 
 
