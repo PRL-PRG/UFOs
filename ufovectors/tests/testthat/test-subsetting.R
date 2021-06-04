9b49d705cab185a4462e59a283d64a5fd74e8a0a
Konrad Siek <konrad.siek@gmail.com>
2021-06-04 17:57:10 +0200

    A small step towards more understandable tests.

--- ufovectors/tests/testthat/test-subsetting.R
+++ ufovectors/tests/testthat/test-subsetting.R
@@ -1,13 +1,11 @@
 context("UFO vector subscripting")
 
-test_that("ufo integer subset", {
-  data <- 1:100000
-  ufo <- ufo_integer(length(data))
+test_ufo_subset <- function (data, subscript, ufo_constructor) {
+  ufo <- ufo_constructor(length(data))
   ufo[seq_len(length(data))] <- data
-
-  subscript <- 1:1000
-
   result <- ufovectors::ufo_subset(ufo, subscript)
-
   expect_equal(result, data[subscript])
-})
+}
+
+test_that("ufo integer subset", {test_ufo_subset(data=as.integer(1:100000), subscript=1:1000, ufo_integer)})
+test_that("ufo numeric subset", {test_ufo_subset(data=as.numeric(1:100000), subscript=1:1000, ufo_numeric)})
