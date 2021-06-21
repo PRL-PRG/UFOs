b6ecc26839c81f4e4644af42dea0fd5a3857bf8d
Konrad Siek <konrad.siek@gmail.com>
2021-06-21 12:27:59 +0200

    Integer subsetting

--- ufovectors/tests/testthat/test-subsetting.R
+++ ufovectors/tests/testthat/test-subsetting.R
@@ -1,4 +1,4 @@
-context("UFO vector subscripting")
+context("UFO vector subsetting")
 
 test_ufo_subset <- function (data, subscript, ufo_constructor) {
   ufo <- ufo_constructor(length(data))
@@ -7,5 +7,23 @@ test_ufo_subset <- function (data, subscript, ufo_constructor) {
   expect_equal(result, data[subscript])
 }
 
-test_that("ufo integer subset", {test_ufo_subset(data=as.integer(1:100000), subscript=1:1000, ufo_integer)})
-test_that("ufo numeric subset", {test_ufo_subset(data=as.numeric(1:100000), subscript=1:1000, ufo_numeric)})
+test_that("ufo integer subset: NULL",       {test_ufo_subset(data=as.integer(1:100000), subscript=NULL,             ufo_integer)})
+test_that("ufo integer subset: 0",          {test_ufo_subset(data=as.integer(1:100000), subscript=0,                ufo_integer)})
+test_that("ufo integer subset: 1",          {test_ufo_subset(data=as.integer(1:100000), subscript=1,                ufo_integer)})
+test_that("ufo integer subset: N/2",        {test_ufo_subset(data=as.integer(1:100000), subscript=50000,            ufo_integer)})
+test_that("ufo integer subset: N",          {test_ufo_subset(data=as.integer(1:100000), subscript=100000,           ufo_integer)})
+test_that("ufo integer subset: 1:10",       {test_ufo_subset(data=as.integer(1:100000), subscript=1:10,             ufo_integer)})
+test_that("ufo integer subset: 1:1000",     {test_ufo_subset(data=as.integer(1:100000), subscript=1:1000,           ufo_integer)})
+test_that("ufo integer subset: 1:N",        {test_ufo_subset(data=as.integer(1:100000), subscript=1:100000,         ufo_integer)})
+test_that("ufo integer subset: N/2:N",      {test_ufo_subset(data=as.integer(1:100000), subscript=50000:100000,     ufo_integer)})
+test_that("ufo integer subset: 2*(2:N/2)",  {test_ufo_subset(data=as.integer(1:100000), subscript=2*(1:50000),      ufo_integer)})
+test_that("ufo integer subset: -1",         {test_ufo_subset(data=as.integer(1:100000), subscript=-1,               ufo_integer)})
+test_that("ufo integer subset: -(N/2)",     {test_ufo_subset(data=as.integer(1:100000), subscript=-50000,           ufo_integer)})
+#test_that("ufo integer subset: -N",         {test_ufo_subset(data=as.integer(1:100000), subscript=-100000,          ufo_integer)})
+test_that("ufo integer subset: -(1:10)",    {test_ufo_subset(data=as.integer(1:100000), subscript=-(1:10),          ufo_integer)})
+test_that("ufo integer subset: -(1:1000)",  {test_ufo_subset(data=as.integer(1:100000), subscript=-(1:1000),        ufo_integer)})
+test_that("ufo integer subset: -(1:N)",     {test_ufo_subset(data=as.integer(1:100000), subscript=-(1:100000),      ufo_integer)})
+#test_that("ufo integer subset: -(N/2:N)",   {test_ufo_subset(data=as.integer(1:100000), subscript=-(50000:100000),  ufo_integer)})
+#test_that("ufo integer subset: -2*(2:N/2)", {test_ufo_subset(data=as.integer(1:100000), subscript=-2*(1:50000),     ufo_integer)})
+
+#test_that("ufo numeric subset", {test_ufo_subset(data=as.numeric(1:100000), subscript=1:1000, ufo_numeric)})
